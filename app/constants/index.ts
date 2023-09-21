a41a4a376eb3c7845d6e22aa7fa413f82cc3ce16
Sepehr Tavakoli <sepehrtvk2001@gmail.com>
2023-09-21 23:47:21 +0330

    add amata

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,9 +1,10 @@
-export const CAFE_NAMES = ["Decopazh", "Manobabam", "Note"];
+export const CAFE_NAMES = ["Decopazh", "Manobabam", "Note", "Amata"];
 export const TIP_CONSTANTS = ["5000", "10000", "15000", "20000"];
 export const LOGIN_PASS = [
   { cafeName: "Decopazh", username: "Decopazh", password: "12345678" },
   { cafeName: "Manobabam", username: "Manobabam", password: "12345678" },
   { cafeName: "Note", username: "Note", password: "12345678" },
+  { cafeName: "Amata", username: "Amata", password: "12345678" },
 ];
 export const WageCostPercent = 5;
 
